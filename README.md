038179b9a022de68984f39b36671f6111143da8a
Sandor Semsey <sandor@es-progress.hu>
2021-12-06 15:55:02 +0100

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# action-provider-addon
+
+The extension is licensed under [AGPL-3.0](LICENSE.txt).
+
+## Requirements
+
+* PHP v7.3+
+* CiviCRM (v5.37+)
+
+## Installation
+
+Sysadmins and developers may clone the [Git](https://en.wikipedia.org/wiki/Git) repo for this extension and
+install it with the command-line tool [cv](https://github.com/civicrm/cv).
+
+```bash
+git clone https://github.com/reflexive-communications/action-provider-addon.git
+cv en action_provider_addon
+```
---
+++ action_provider_addon.php
@@ -0,0 +1,184 @@
+<?php
+
+require_once 'action_provider_addon.civix.php';
+
+// phpcs:disable
+use CRM_ActionProviderAddon_ExtensionUtil as E;
+
+// phpcs:enable
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_config/
+ */
+function action_provider_addon_civicrm_config(&$config)
+{
+    _action_provider_addon_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_xmlMenu().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_xmlMenu
+ */
+function action_provider_addon_civicrm_xmlMenu(&$files)
+{
+    _action_provider_addon_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
+ */
+function action_provider_addon_civicrm_install()
+{
+    _action_provider_addon_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
+ */
+function action_provider_addon_civicrm_postInstall()
+{
+    _action_provider_addon_civix_civicrm_postInstall();
+}
+
+/**
+ * Implements hook_civicrm_uninstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
+ */
+function action_provider_addon_civicrm_uninstall()
+{
+    _action_provider_addon_civix_civicrm_uninstall();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
+ */
+function action_provider_addon_civicrm_enable()
+{
+    _action_provider_addon_civix_civicrm_enable();
+}
+
+/**
+ * Implements hook_civicrm_disable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
+ */
+function action_provider_addon_civicrm_disable()
+{
+    _action_provider_addon_civix_civicrm_disable();
+}
+
+/**
+ * Implements hook_civicrm_upgrade().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
+ */
+function action_provider_addon_civicrm_upgrade($op, CRM_Queue_Queue $queue = null)
+{
+    return _action_provider_addon_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implements hook_civicrm_managed().
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
+ */
+function action_provider_addon_civicrm_managed(&$entities)
+{
+    _action_provider_addon_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implements hook_civicrm_caseTypes().
+ *
+ * Add CiviCase types provided by this extension.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_caseTypes
+ */
+function action_provider_addon_civicrm_caseTypes(&$caseTypes)
+{
+    _action_provider_addon_civix_civicrm_caseTypes($caseTypes);
+}
+
+/**
+ * Implements hook_civicrm_angularModules().
+ *
+ * Add Angular modules provided by this extension.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_angularModules
+ */
+function action_provider_addon_civicrm_angularModules(&$angularModules)
+{
+    // Auto-add module files from ./ang/*.ang.php
+    _action_provider_addon_civix_civicrm_angularModules($angularModules);
+}
+
+/**
+ * Implements hook_civicrm_alterSettingsFolders().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_alterSettingsFolders
+ */
+function action_provider_addon_civicrm_alterSettingsFolders(&$metaDataFolders = null)
+{
+    _action_provider_addon_civix_civicrm_alterSettingsFolders($metaDataFolders);
+}
+
+/**
+ * Implements hook_civicrm_entityTypes().
+ *
+ * Declare entity types provided by this module.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+ */
+function action_provider_addon_civicrm_entityTypes(&$entityTypes)
+{
+    _action_provider_addon_civix_civicrm_entityTypes($entityTypes);
+}
+
+/**
+ * Implements hook_civicrm_themes().
+ */
+function action_provider_addon_civicrm_themes(&$themes)
+{
+    _action_provider_addon_civix_civicrm_themes($themes);
+}
+
+// --- Functions below this ship commented out. Uncomment as required. ---
+
+/**
+ * Implements hook_civicrm_preProcess().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_preProcess
+ */
+//function action_provider_addon_civicrm_preProcess($formName, &$form) {
+//
+//}
+
+/**
+ * Implements hook_civicrm_navigationMenu().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_navigationMenu
+ */
+//function action_provider_addon_civicrm_navigationMenu(&$menu) {
+//  _action_provider_addon_civix_insert_navigation_menu($menu, 'Mailings', [
+//    'label' => E::ts('New subliminal message'),
+//    'name' => 'mailing_subliminal_message',
+//    'url' => 'civicrm/mailing/subliminal',
+//    'permission' => 'access CiviMail',
+//    'operator' => 'OR',
+//    'separator' => 0,
+//  ]);
+//  _action_provider_addon_civix_navigationMenu($menu);
+//}
